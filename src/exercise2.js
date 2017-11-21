b4d5b3e18b04942b36316e5e66a9c8efc484a819
AndreiYnov <andrei.radocea@ynov.com>
2017-11-21 17:08:41 +0100

    Exercice 2 Fin

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,8 +1,7 @@
 exports.run = function(input) {
   var plusgrandnombre = 0;
-    for(var i=0; i<input.length;i++)
-        {
-            
+    for(var i=0; i<input.length;i++){
+        
         }
     return plusgrandnombre;
 };
